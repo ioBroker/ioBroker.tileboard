fd4af31a48499d09716d2ed0401c6255674a513e
GermanBluefox <dogafox@gmail.com>
2020-08-22 21:43:57 +0200

    ### 0.3.1 (2020-08-22) * (bluefox) The compatibility to socket.io 3.0.13 provided

--- www/scripts/vendors/conn.js
+++ www/scripts/vendors/conn.js
@@ -476,8 +476,8 @@ var servConn = {
     getVersion:       function (callback) {
         if (!this._checkConnection('getVersion', arguments)) return;
 
-        this._socket.emit('getVersion', function (version) {
-            if (callback) callback(version);
+        this._socket.emit('getVersion', function (error, version) {
+            callback && callback(version || error);
         });
     },
     subscribe:        function (idOrArray, callback) {
@@ -500,9 +500,9 @@ var servConn = {
             console.log('socket.io not initialized');
             return;
         }
-        this._socket.emit('getVersion', function (version) {
-            if (callback)
-                callback(version);
+        this._socket.emit('getVersion', function (error, version) {
+            callback && callback(version || error);
+
         });
     },
     readFile:         function (filename, callback, isRemote) {
